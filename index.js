0ae18d22696ce37482c454851dbf95bcb4a6cdb9
Stephen Whitmore <stephen.whitmore@gmail.com>
2015-05-29 22:19:43 -0700

    Stream bugfix; caching on mplayer.

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 var net = require('net');
 var run = require('comandante');
 var combiner = require('stream-combiner2');
+var through = require('through2');
 
 net.createServer(function(clientStream) {
   handler(clientStream);
 }).listen(5000);
 
-function handler(stream) {
-  var cmd = run('mplayer', ['-']);
-  var stream = combiner(stream, cmd, stream);
+function handler(clientStream) {
+  var cmd = run('mplayer', ['-cache', '8192', '-']);
+
+  var stream = combiner(clientStream, through(function(data, enc, callback) {
+    console.log(data.length);
+    this.push(data);
+    callback();
+  }), cmd, clientStream);
+
   stream.on('end', function() {
     console.log('ended');
     cmd.kill();
   });
-  stream.on('error', function(e) {
-    console.log('ended due to error ' + e);
+  stream.on('close', function() {
+    console.log('closed');
     cmd.kill();
+  });
+  stream.on('error', function(e) {
+    console.log('error: ' + e);
+    cmd.kill();
+  });
+  stream.on('finish', function() {
+    console.log('finish');
+  });
+  stream.on('pipe', function() {
+    console.log('pipe');
+  });
+  stream.on('unpipe', function() {
+    console.log('unpipe');
   });
 }
 
--- stream.js
+++ stream.js
@@ -1,5 +1,25 @@
 var net = require('net');
+var combiner = require('stream-combiner2');
 
 var serverStream = net.connect(5000, 'localhost', function() {
-  process.stdin.pipe(serverStream).pipe(process.stdout);
+  var stream = combiner(process.stdin, serverStream, process.stdout);
+
+  stream.on('error', function(e) {
+    console.log('ended due to error ' + e);
+  });
+  stream.on('end', function(e) {
+    console.log('ended');
+  });
+  stream.on('close', function() {
+    console.log('closed');
+  });
+  stream.on('finish', function() {
+    console.log('finish');
+  });
+  stream.on('pipe', function() {
+    console.log('pipe');
+  });
+  stream.on('unpipe', function() {
+    console.log('unpipe');
+  });
 });
