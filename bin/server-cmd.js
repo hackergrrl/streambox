ead322f56e61475f2696c945326302cc08d1c0d5
Stephen Whitmore <stephen.whitmore@gmail.com>
2015-05-30 17:20:06 -0700

    External endpoint loading. Not that it does anything yet.

--- bin/server-cmd.js
+++ bin/server-cmd.js
@@ -1,14 +1,44 @@
 #!/usr/bin/env node
 
-var path = require('path');
+var fs = require('fs');
 var defined = require('defined');
+var path = require('path');
 var net = require('net');
 
 var HOME = defined(process.env.HOME, process.env.USERDIR);
-var DIR = defined(process.env.STREAMBOX_PATH, path.join(HOME, '.config/streambox'));
+var DIR = defined(
+  process.env.STREAMBOX_PATH,
+  path.join(HOME, '.config/streambox'),
+  path.join(__dirname, '..'));
+
+var endpoints = {};
+loadEndpoints(path.join(DIR, 'node_modules'));
+
 
 // Load installed stream endpoints
-// ...
+function loadEndpoints(endpointsDir) {
+  if (!fs.existsSync(endpointsDir)) {
+    console.error('No endpoints found. Your server won\'t be able to handle any streams.');
+  } else {
+    var files = fs.readdirSync(endpointsDir);
+    for (var i in files) {
+      var name = files[i];
+      if (name.indexOf('streambox-') !== 0) {
+        continue;
+      }
+      var jsPath = path.join(endpointsDir, name, 'index');
+      if (!fs.existsSync(jsPath + '.js')) {
+        continue;
+      }
+      var endpoint = require(jsPath);
+      for (var i in endpoint.endpoints) {
+        var endpointName = endpoint.endpoints[i];
+        endpoints[endpointName] = endpoint;
+        console.error(endpointName + " => " + name + '  (via ' + endpointsDir + ')');
+      }
+    }
+  }
+}
 
 net.createServer(function(clientStream) {
 }).listen(5000);
